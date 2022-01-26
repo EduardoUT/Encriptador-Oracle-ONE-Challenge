8ced170aac5ba13b96a17e2332d6c8d8d5045029
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-01-25 19:53:54 -0600

    Convirtiendo la función desencriptarMensaje a una función anónima asociandola directamente a un evento click del boton desencriptar

--- js/desencriptar.js
+++ js/desencriptar.js
@@ -1,14 +1,19 @@
-function desencriptarMensaje() {
-    var mensaje = document.querySelector("#input-texto").value;
+var botonDesencriptar = document.querySelector("#btn-desencriptar");
+
+botonDesencriptar.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    var mensaje = document.querySelector("#input-texto");
+    var valorMensaje = mensaje.value;
+    var campoDos = document.querySelector("#msg");
     var decript = "";
 
-    decript= mensaje.replace(/ai/g, letras[0]).replace(/enter/g, letras[1]).
+    decript= valorMensaje.replace(/ai/g, letras[0]).replace(/enter/g, letras[1]).
     replace(/imes/g, letras[2]).replace(/ober/g, letras[3]).replace(/ufat/g, letras[4]);
-    
         
-    console.log("Desencriptado: " + decript);
-    document.getElementById("msg").value = decript;
+    campoDos.value = decript;
 
     limpiarCampoUno();
 
-}
+});
+
