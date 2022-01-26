87d95b0a6bee534ae1b3d76be95e4d5f4cb2619c
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-01-25 19:44:26 -0600

    Cortando la función desencriptar

--- js/script.js
+++ js/script.js
@@ -28,23 +28,6 @@ var inputUno = document.querySelector("#input-texto");
 var inputDos = document.querySelector("#msg");
 inputUno.focus();
 
-
-
-function desencriptarMensaje() {
-    var mensaje = document.querySelector("#input-texto").value;
-    var decript = "";
-
-    decript= mensaje.replace(/ai/g, letras[0]).replace(/enter/g, letras[1]).
-    replace(/imes/g, letras[2]).replace(/ober/g, letras[3]).replace(/ufat/g, letras[4]);
-    
-        
-    console.log("Desencriptado: " + decript);
-    document.getElementById("msg").value = decript;
-
-    limpiarCampoUno();
-
-}
-
 function copiarMensaje() {
     var copiarTexto = document.querySelector("#msg");
     copiarTexto.select();
