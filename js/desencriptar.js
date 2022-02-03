c3cf015eb7778ff7ee7e5848f0bba186ba569661
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-02-03 16:05:28 -0600

    Realizando pruebas de desencriptacion

--- js/desencriptar.js
+++ js/desencriptar.js
@@ -9,17 +9,17 @@ No se permite acentuación de palabras
 */
 var botonDesencriptar = document.querySelector("#btn-desencriptar");
 
-botonDesencriptar.addEventListener("click", function(event) {
+botonDesencriptar.addEventListener("click", function (event) {
     event.preventDefault();
-    
+
     var mensaje = document.querySelector("#input-texto");
     var valorMensaje = mensaje.value;
     var campoDos = document.querySelector("#msg");
     var decript = "";
-
-    decript= valorMensaje.replace(/ai/g, letras[0]).replace(/enter/g, letras[1]).
-    replace(/imes/g, letras[2]).replace(/ober/g, letras[3]).replace(/ufat/g, letras[4]);
-        
+    
+    decript = valorMensaje.replace(/ai/g, letras[0]).replace(/enter/g, letras[1]).
+    replace(/imes/g, letras[2]).replace(/ober/g, letras[3]).replace(/ufat/g, letras[4]);    
+    //console.log(decript);
     campoDos.value = decript;
 
     limpiarCampoUno();
